cd2c849fc53e92947c234169e129238b4cc2e42a
George Adamopoulos <30707361+GeorgeAdamon@users.noreply.github.com>
2024-01-27 22:09:02 +0000

    Updates after GAME2 class

--- VFX_Graph_Samples/Assets/Scripts/ExampleReceivers/Vector3Receiver.cs
+++ VFX_Graph_Samples/Assets/Scripts/ExampleReceivers/Vector3Receiver.cs
@@ -10,4 +10,22 @@ public class Vector3Receiver : MonoBehaviour, IValue<Vector3>
         get => value;
         set => this.value = value;
     }
+
+    public float X
+    {
+        get => value.x;
+        set => this.value.x = value;
+    }
+    
+    public float Y
+    {
+        get => value.y;
+        set => this.value.y = value;
+    }
+    
+    public float Z
+    {
+        get => value.z;
+        set => this.value.z = value;
+    }
 }
